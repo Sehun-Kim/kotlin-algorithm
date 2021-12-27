d25540d27ab4c61440992b3dfafd902241912ad3
sehun <sechun0215@gmail.com>
2021-12-27 20:06:53 +0900

    change num

--- src/main/kotlin/programmers/level1/FindLeftOneNumber.kt
+++ src/main/kotlin/programmers/level1/FindLeftOneNumber.kt
@@ -5,7 +5,7 @@ class FindLeftOneNumber {
     fun solution(n: Int): Int {
         val num = (n - 1)
 
-        for (i: Int in 2..n) {
+        for (i: Int in 2..num) {
             if (num % i == 0) return i
         }
 
